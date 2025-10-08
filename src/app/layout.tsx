5ae67bfa30630d82beed3484f01541225ab6bd15
gahette <gahette@icloud.com>
2025-10-08 18:53:37 +0200

    Reformat code and add authentication UI
    This changes apply consistent code formatting and implement custom
    authentication UI components, replacing the default Clerk providers.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,22 @@
-import './globals.css'
-import {Metadata} from "next";
-import {ClerkProvider} from "@clerk/nextjs";
-
+import "./globals.css";
+import { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
-    title: "Lama Dev X Clone",
-    description: "Next.js social media application project",
+  title: 'Lama Dev X Clone',
+  description: 'Next.js social media application project',
 }
 
 export default function AppLayout({
-                                      children,
-                                  }: Readonly<{
-    children: React.ReactNode
+  children,
+}: Readonly<{
+  children: React.ReactNode
 }>) {
-    return (
-        <ClerkProvider>
-            <html lang='en'>
-            <body>
-            {children}
-            </body>
-            </html>
-        </ClerkProvider>
-
-    )
+  return (
+    <ClerkProvider>
+      <html lang='en'>
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
+  )
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,9 @@
-import {clerkMiddleware, createRouteMatcher} from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher("/")
+const isProtectedRoute = createRouteMatcher('/')
 
-export default clerkMiddleware(async (auth, req) => {
+export default clerkMiddleware(
+  async (auth, req) => {
     if (isProtectedRoute(req)) await auth.protect()
 }, {
     signInUrl: "/sign-in",
@@ -10,10 +11,10 @@ export default clerkMiddleware(async (auth, req) => {
 })
 
 export const config = {
-    matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
-        '/(api|trpc)(.*)',
-    ],
-};
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}

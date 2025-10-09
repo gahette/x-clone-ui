2699ce1dad7a5c9dc166dc46c2879a695ecd7468
gahette <gahette@icloud.com>
2025-10-09 15:53:26 +0200

    Add user feed filtering and following functionality
    Shows posts from current user and followed accounts in main feed, or filters to
    show only a specific user's posts when viewing their profile

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,17 +1,44 @@
-import React from 'react';
-import {prisma} from "@/prisma";
-import Post from "@/components/Post";
+import React from 'react'
+import { prisma } from '@/prisma'
+import Post from '@/components/Post'
+import { auth } from '@clerk/nextjs/server'
 
-const Feed = async () => {
-    const posts = await prisma.post.findMany()
-    return (
-        <div className="">
-            {posts.map((post) => (
-                <div key={post.id}><Post/></div>
+const Feed = async ({ userProfiledId }: { userProfiledId: string }) => {
+  const { userId } = await auth()
 
-            ))}
+  if (!userId) return
+
+  const whereCondition = userProfiledId
+    ? { parentPostId: null, userId: userProfiledId }
+    : {
+        parentPostId: null,
+        userId: {
+          in: [
+            userId,
+            ...(
+              await prisma.follow.findMany({
+                where: { followerId: userId },
+                select: { followingId: true },
+              })
+            ).map((follow) => follow.followingId),
+          ],
+        },
+      }
+
+  const posts = await prisma.post.findMany({ where: whereCondition })
+
+  console.log(posts)
+
+  // FETCH POSTS FROM THE CURRENT USER AND THE FOLLOWINGS
+  return (
+    <div className=''>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <Post />
         </div>
-    );
-};
+      ))}
+    </div>
+  )
+}
 
-export default Feed;
+export default Feed
